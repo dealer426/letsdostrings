1a5c124012c4d01a04627cdd5fe704a811f60c2e
dealer426 <burnssamuel01@gmail.com>
2020-01-15 14:52:42 -0500

    Initial Upload of Application

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace letsDoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Your First Name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Last Name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine(firstName + " " + lastName);
+
+            int firstNameLength = firstName.Length;
+            int lastNameLength = lastName.Length;
+            Console.WriteLine("Your first name has {0} chars and your last name has {1} chars", firstNameLength, lastNameLength);
+
+            Console.WriteLine("Please answer the riddle below");
+            Console.WriteLine("Box without latches, key or a lid, but golden treasure inside is hid.");
+            string riddleAnswer = Console.ReadLine();
+
+            if(riddleAnswer.ToLower() == "egg")
+            {
+                Console.WriteLine("You guess correct!");
+            }
+
+            else
+            {
+                Console.WriteLine("I will show you the way out!");  
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
